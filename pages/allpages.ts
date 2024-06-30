446977d98630ccb2b8adc763aa20e99fe1a64090
Zuzana Přichystalová <zuzi.prichystalova@gmail.com>
2024-06-30 10:22:16 +0200

    Adding new pages
    Pages Buttons, Cats, Login, Hover, Modal, Selectors.

--- pages/allpages.ts
+++ pages/allpages.ts
@@ -1,16 +1,29 @@
 import { Page } from "@playwright/test";
 import { ButtonPage } from "./buttons";
 import { CatsPage } from "./cats";
+import { LoginPage } from "./login";
+import { HoverPage } from "./hover";
+import { SelectorsPage } from "./selectors";
+import { ModalPage } from "./modal";
 
 export class AllPages {
   readonly buttonPage: ButtonPage;
   readonly catsPage: CatsPage;
+  readonly LoginPage: LoginPage;
+  readonly HoverPage: HoverPage;
+  readonly SelectorsPage: SelectorsPage;
+  readonly ModalPage: ModalPage;
   readonly page: Page;
+  
 
   constructor(page: Page) {
     this.page = page;
     this.buttonPage = new ButtonPage(page);
     this.catsPage = new CatsPage(page);
+    this.LoginPage = new LoginPage(page);
+    this.HoverPage = new HoverPage(page);
+    this.SelectorsPage = new SelectorsPage(page);
+    this.ModalPage = new ModalPage(page);
   }
 
   async visit(url: string) {
