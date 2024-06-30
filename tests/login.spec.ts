f905fa8a1c229f3c340cb3afe225466ce44ed354
Zuzana Přichystalová <zuzi.prichystalova@gmail.com>
2024-06-30 10:33:49 +0200

    Writing tests for loginPage

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -7,10 +7,78 @@ test.describe("Login", () => {
 
   test.beforeEach(async ({ page }) => {
     pages = new AllPages(page);
-    await pages.visit("/login.html");
+    await page.goto("https://automat.ostrava.digital/login.html");
   });
 
-  test("template for you", async ({ page }) => {
-    //pises kod sem :)
+  test("log in - happy path, obrázek jde vidět", async ({ page }) => {
+    await pages.LoginPage.inputUsername.fill("czechitas");
+    await pages.LoginPage.inputPassword.fill("budoucnost");
+    await pages.LoginPage.buttonLogIn.click();
+
+    await expect(pages.LoginPage.imageOwl).toBeVisible();
+
+  });
+
+  test("log in - happy path, objeví se nápis LOGGED!", async ({ page }) => {
+    await pages.LoginPage.inputUsername.fill("czechitas");
+    await pages.LoginPage.inputPassword.fill("budoucnost");
+    await pages.LoginPage.buttonLogIn.click();
+
+    await expect(pages.LoginPage.loggedHeader).toBeVisible();
+
+  });
+
+  test("log in - happy path, objeví se nápis, který má text LOGGED!", async ({ page }) => {
+    await pages.LoginPage.inputUsername.fill("czechitas");
+    await pages.LoginPage.inputPassword.fill("budoucnost");
+    await pages.LoginPage.buttonLogIn.click();
+
+    await expect(pages.LoginPage.loggedHeader).toHaveText("LOGGED!");
+
+  });
+
+  test("log in - happy path, objeví se odstavec", async ({ page }) => {
+    await pages.LoginPage.inputUsername.fill("czechitas");
+    await pages.LoginPage.inputPassword.fill("budoucnost");
+    await pages.LoginPage.buttonLogIn.click();
+
+    await expect(pages.LoginPage.paragraph).toBeVisible();
+
+  });
+
+  test("log in - happy path, objeví se odstavec s textem", async ({ page }) => {
+    await pages.LoginPage.inputUsername.fill("czechitas");
+    await pages.LoginPage.inputPassword.fill("budoucnost");
+    await pages.LoginPage.buttonLogIn.click();
+
+    await expect(pages.LoginPage.paragraph).toHaveText("Jsi přihlášená! Dobrá práce! Tak. Teď ještě doprogramovat ověření, že jsi přihlášená.");
+
+  });
+
+  test("log in, invalid password, inputPassword to be empty", async ({ page }) => {
+    await pages.LoginPage.inputUsername.fill("czechitas");
+    await pages.LoginPage.inputPassword.fill("123456");
+    await pages.LoginPage.buttonLogIn.click();
+
+    await expect(pages.LoginPage.inputPassword).toBeEmpty();
+
+  });
+
+  test("log in, invalid password, toast to be visible", async ({ page }) => {
+    await pages.LoginPage.inputUsername.fill("czechitas");
+    await pages.LoginPage.inputPassword.fill("123456");
+    await pages.LoginPage.buttonLogIn.click();
+
+    await expect(pages.LoginPage.errorToast).toBeVisible();
+
+  });
+
+  test("log in, invalid password, toast to Have text", async ({ page }) => {
+    await pages.LoginPage.inputUsername.fill("czechitas");
+    await pages.LoginPage.inputPassword.fill("123456");
+    await pages.LoginPage.buttonLogIn.click();
+
+    await expect(pages.LoginPage.errorToast).toHaveText("Špatné heslo!");
+
   });
 });
